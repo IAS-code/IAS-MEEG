331df64b54b2e253ced8a76684d3974ea72bb878
annapasca <a.pascarella@iac.cnr.it>
2019-04-04 15:52:10 +0200

    add script demo

---
+++ IAS_Demo.m
@@ -0,0 +1,81 @@
+%% IAS_Demo
+% This is a demo for testing the Iterative Alternating Sequential (IAS) algorithm. 
+% 
+% The real data set used for this demo is the MEG sample data acquired with the Neuromag Vectorview system 
+% at MGH/HMS/MIT Athinoula A. Martinos Center Biomedical Imaging and made available, together with the 
+% MRI reconstructions created with FreeSurfer, in the MNE software package (http://martinos.org/mne). 
+% As part of the protocol for the data collection, checkerboard patterns were presented into the left 
+% and right visual field, interspersed by tones to the left or right ear. The interval between the stimuli 
+% was 750 ms. Occasionally a smiley face was presented at the center of the visual field. The subject 
+% was asked to press a key with the right index finger as soon as possible after the appearance of the face 
+% [A. Gramfort et al, MNE software for processing MEG and EEG data, Neuroimage 86 (2014) 446-460].
+% In the test we only consider the trials corresponding to the left-ear auditory stimulus 
+% and perform the averaging on these trials. 
+% The source space containing both cortical regions and subcortical structures
+% after discretization comprises 22019 vertices.
+%
+%---------------------------------------------------------------
+% References:
+%
+% D. Calvetti, A. Pascarella, F. Pitolli, E. Somersalo, B. Vantaggi
+% A hierarchical Krylov-Bayes iterative inverse solver for MEG with physiological preconditioning
+% Inverse Problems, 31 (12) (2015) 12500
+% 
+% D. Calvetti, A. Pascarella, F. Pitolli, E. Somersalo, B. Vantaggi
+% Brain activity mapping from MEG data via a hierarchical Bayesian algorithm 
+% with automatic depth weighting: sensitivity and specificity analysis
+% arXiv:1707.05639 (2017)
+%----------------------------------------------------------------
+% Version: July, 2017
+%----------------------------------------------------------------
+
+%% Reset all
+clear; clc; 
+close all
+
+%% Input
+%     SNR: estimated signal-to-noise ratio
+SNR = 25;
+%     eta: scalar, parameter for selecting the focality of the reconstructed activity
+eta = 0.001;
+
+%% Loading the source space
+%     coord, normals: (3,N) array, coordinates and normal vectors of the dipoles in the grid
+disp('Loading source space')
+load('SourceSpace')
+
+%% Loading the leadfield matrix
+%     LF: (M,3*N) array, the lead field matrix (M is the number of channels; N is the number of dipoles)
+disp('Loading leadfield matrix')
+load('LeadfieldMatrix')
+
+%% Loading the magnetic data
+%     data: (M,T) array, a set of data of length T
+%     time: T-vector, times 
+disp('Loading data')
+load('visualMEGData')
+
+%% Building anatomical prior
+disp('Building anatomical prior')
+APChol = BuildAnatomicalPrior(coord,normals);
+
+%% Setting theta_star and scaling 
+disp('Setting parameters')
+t_peak = 173;   
+t_min = 160;
+t_max = 250;
+B = data(:,t_min:t_max);   % a clip of the data 
+[theta_star,theta_cut_off,sigma,LF_scaling,B_scaling] = SetParameters(LF,B,SNR);
+
+%% Solving the inverse problem using IAS algorithm
+disp(['Running IAS algorithm from time ',num2str(time(t_min)),' ms to time ',num2str(time(t_max)),' ms'])
+Q = IAS_algorithm(LF, LF_scaling, APChol, data, B_scaling, sigma, theta_star, eta);
+
+%% Visualizing the activity map
+disp(['Visualizing activity map at time ',num2str(time(t_peak)),' ms'])
+N = size(LF,2)/3;  % number of dipoles in the source space
+t_vis = t_peak;
+q = Q(:, t_vis);
+dip_norm2 = sum(reshape(q,3,N).^2,1);
+Q_est = sqrt(dip_norm2);
+SlicedVisualization_ActivityMap_Gif(coord,Q_est);
---
+++ SetParameters.m
@@ -0,0 +1,111 @@
+function [theta_star,theta_cut_off,sigma,LF_scaling,B_scaling] = SetParameters(LF,B,SNR,cut_off,is_paint)
+% This function scales the lead field and the data, adjusts the truncation of the sensitivities, and returns an estimate for the standard deviation of the noise
+%
+%  Input:
+%       LF: (m,3*n) array, the lead field matrix where m is the number of channels and n is the number of dipoles
+%
+%       B: (m,T) array, a clip of the data of length T
+%
+%       SNR: estimated signal-to-noise ratio
+%
+%       cut_off: scalar 0<cut_off<1, defines the cut off level, removing (1-cut_off)% of the highest theta_star values. Values of the order 0.8...0.9 are expected for cut_off (default value: 0.9)
+%
+%       is_paint: scalar; setting is_paint=1 plots theta_star (default value: 0)
+%
+%  Output:    
+%       theta_star: n-vector, computed theta_star values with truncation
+%
+%       theta_cut_off: scalar, the cut-off value of the theta_star
+%
+%       sigma: scalar, standard deviation of the scaled noise
+%
+%       LF_scaling: scalar to be used to scale the lead field
+%
+%       B_scaling: scalar, to be used for scaling the data
+%
+%  Usage: 
+%       [theta_star,theta_cut_off,sigma,LF_scaling,B_scaling] = SetParameters(LF,B,SNR,cut_off,is_paint)
+%        SetParameters(LF,B,SNR) is equivalent to SetParameters(LF,B,SNR,0.9,0)
+%
+%        SetParameters(LF,B,SNR,cut_off) is equivalent to SetParameters(LF,B,SNR,cut_off,0)
+%----------------------------------------------------------------
+% References:
+%
+% D. Calvetti, A. Pascarella, F. Pitolli, E. Somersalo, B. Vantaggi
+% A hierarchical Krylov-Bayes iterative inverse solver for MEG with physiological preconditioning
+% Inverse Problems, 31 (12) (2015) 12500
+% 
+% D. Calvetti, A. Pascarella, F. Pitolli, E. Somersalo, B. Vantaggi
+% Brain activity mapping from MEG data via a hierarchical Bayesian algorithm 
+% with automatic depth weighting: sensitivity and specificity analysis
+% arXiv:1707.05639 (2017)
+%----------------------------------------------------------------
+
+% Setting default values
+if nargin == 3, cut_off = 0.9; is_paint = 0; end
+if nargin == 4,                is_paint = 0; end
+
+% Reading the number of channels m and the number of dipoles n
+[m,n3] = size(LF);
+n = n3/3; 
+
+% Computing the sensitivity matrix S: the jth row contains the powers of the
+% magnetic field at jth magnetometer corresponding to unit dipole
+% components at the field points
+AddCols = kron(speye(n),[1;1;1]);
+S = (LF.^2)*AddCols;
+
+% Finding the power of the magnetic field at the most sensitive magnetometer 
+% for each dipole
+MaxS = max(S);
+
+% Scaling the lead field so that the mean maximum sensitivity is a unit
+MeanSens = 1/n*sum(MaxS);
+MaxS = 1/MeanSens*MaxS;
+LF_scaling = 1/sqrt(MeanSens);
+LF = LF_scaling*LF;
+
+% Checking the size of the data
+[m,T] = size(B);
+
+% Finding the maximum value and the norm of the magnetic field
+Bnorm_ave = 1/m*sqrt(sum(B.^2,1))';
+
+% Scaling the magnetic field so that the mean norm is unity
+MeanB = 1/T*sum(Bnorm_ave);
+B_scaling = 1/MeanB;
+B = B_scaling*B;
+
+% Computing theta_star using the scaled fields
+Phi = 1/T*norm(B.^2,'fro');
+S_scaled = (LF.^2)*AddCols;
+Sens = sum(S_scaled,1);
+theta_star = (2/5)*Phi./Sens; % beta = 5/2
+
+% Cutting the highest theta_star values
+Q = theta_star - min(theta_star);
+Qsort = sort(Q,'ascend');
+i_cut = round(cut_off*n);
+theta_cut_off = Qsort(i_cut) + min(theta_star);
+theta_star = min(theta_star,theta_cut_off);
+
+% Plotting the values of theta_star
+if is_paint
+  figure
+  semilogy((1:n),theta_star,'k.','MarkerSize',15)
+  set(gca,'FontSize',20)
+  ylabel('\theta^*,  normalized units','FontSize',20)
+  % Adjusting the cut-off value
+  hold on
+  semilogy([1,n],[theta_cut_off,theta_cut_off],'r-','LineWidth',2)
+  v = axis;
+  axis([1,n,v(3),v(4)]);
+  hold off
+end
+
+% Adjusting the noise level
+ B_ave_power = 1/T*sum(sum(B.^2)); 
+ sigma2 = B_ave_power/(m*SNR);
+ sigma = sqrt(sigma2);
+ 
+ 
